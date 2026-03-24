5dce7eaa4eec020a9926a67402bb4bd194bf25db
SeanLiu8800 <tyson9963@gmail.com>
2026-03-23 18:22:38 -0700

    Created basic Dashing
    -trying to figure out how to decrease the dash speed convincingly during a dash

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,18 +3,6 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private PlayerMovement movement;
-    void Start()
-    {
-        moveAction = InputSystem.actions.FindAction("Move");
-    }
-    void Update()
-    {
-        MoveCharacter();
-    }
-    private InputAction moveAction;
-    private void MoveCharacter()
-    {
-        movement.MovePlayer(moveAction.ReadValue<Vector2>().normalized);
-    }
+    [field : HideInInspector] public PlayerMovement movement { get; private set; }
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : PlayerComponent
 {
     [SerializeField] private Collider2D playerCollider;
     [SerializeField] private Rigidbody2D playerRigidbody;
 
-    [SerializeField, Range(0.0f, 5.0f)] float movementSpeed = 5.0f;
+    private InputAction moveAction;
+    [SerializeField, Range(0.0f, 10.0f)] float moveSpeed = 5.0f;
+    private Vector3 lastmovementDirection = Vector3.up;
+    private Vector3 movementInput;
+
+    private bool isDashing = false;
+    private float dashStartTime = 0.0f;
+    [SerializeField, ReadOnly] private float currentDashTime = 0.0f;
+    [SerializeField, Range(0.0f, 1.5f)] private float dashTime = 2.0f;
+    private Vector3 dashDirection;
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+    void Update()
+    {
+        MoveCharacter();
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) StartDash();
+        else if (Keyboard.current.spaceKey.wasReleasedThisFrame) StopDash();
+    }
     void FixedUpdate()
     {
-        playerRigidbody.linearVelocity = movementInput * movementSpeed;
+        playerRigidbody.linearVelocity = movementInput * moveSpeed;
+        if (isDashing) Dash();
     }
-
-    private Vector3 movementInput;
-    public void MovePlayer(Vector3 input)
+    private void MoveCharacter()
     {
-        movementInput = input;
+        movementInput = moveAction.ReadValue<Vector2>();
+        if (movementInput != Vector3.zero) lastmovementDirection = movementInput;
+    }
+    
+    private void StartDash()
+    {
+        isDashing = true;
+        dashStartTime = Time.time;
+        dashDirection = (movementInput == Vector3.zero) ? lastmovementDirection : movementInput;
+    }
+    private void Dash()
+    {
+        currentDashTime = Time.time - dashStartTime;
+        if (currentDashTime >= dashTime)
+        {
+            StopDash();
+            return;
+        }
+        float coef = currentDashTime / dashTime;
+        playerRigidbody.AddForce(Vector3.Lerp(dashDirection * 1000, Vector3.zero, coef*coef), ForceMode2D.Force);
+    }
+    private void StopDash()
+    {
+        isDashing = false;
     }
 }
