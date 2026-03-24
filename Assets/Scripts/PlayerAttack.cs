a8df8eba3d96ef95b37244756f66884fe765b31a
SeanLiu8800 <tyson9963@gmail.com>
2026-03-24 02:00:45 -0700

    Attack now detects damageable stuff in collider
    -also includes a ContactFilter2D to filter out what is/ isn't targetable

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,26 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 public class PlayerAttack : PlayerComponent
 {
+    private InputAction attackAction;
     [SerializeField] private Collider2D attackArea;
+    [SerializeField] private ContactFilter2D attackTargetFiler;
     protected override void Awake()
     {
         base.Awake();
+
+        attackAction = InputSystem.actions.FindAction("Attack");
     }
     void OnEnable()
     {
-        
+        attackAction.canceled += Attack;
     }
     void OnDisable()
     {
-        
-    }
-    void Update()
-    {
-        if (Keyboard.current?.spaceKey.wasReleasedThisFrame == true)
-        {
-            Debug.Log("ATTACK");
-        }
+        attackAction.canceled -= Attack;
     }
     void FixedUpdate()
     {
@@ -29,6 +27,15 @@ public class PlayerAttack : PlayerComponent
 
     private void Attack(InputAction.CallbackContext context)
     {
-
+        Debug.Log("Player attempts attack");
+        List<Collider2D> hits = new List<Collider2D>();
+        if (attackArea.Overlap(attackTargetFiler, hits) > 0)
+        {
+            Debug.Log("Player Attacks!");
+            foreach (Collider2D currCollider in hits)
+            {
+                Debug.Log($"Hit {currCollider.name}");
+            }
+        }
     }
 }
