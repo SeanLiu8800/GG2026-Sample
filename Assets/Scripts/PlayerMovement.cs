eb14ce7af0ed60c394b376b58a85b71a3db7a586
SeanLiu8800 <tyson9963@gmail.com>
2026-03-23 22:15:41 -0700

    Refined Dash
    -can now Perfect Dash, which increases current move Speed by 1.5x
    -Imperfect Dashes reduce the current move speed by 5 units, to a minimum of half of the original moveSpeed
    -Current Move Speed now corrects back to moveSpeed based on speedRestoreRate and speedDecayRate times a multiplier determined by how much speed is over moveSpeed
    -Dash has a cooldown determined by dashCooldown (Perfect Dashes halves the cooldown)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,23 @@ public class PlayerMovement : PlayerComponent
 
     private InputAction moveAction;
     [SerializeField, Range(0.0f, 10.0f)] float moveSpeed = 5.0f;
+    [SerializeField, ReadOnly, Range(0.0f, 60.0f)] float currMoveSpeed = 0.0f;
+    [SerializeField, ReadOnly, Range(5.0f, 60.0f)] float maxMoveSpeed = 30.0f;
     private Vector3 lastmovementDirection = Vector3.up;
     private Vector3 movementInput;
 
     private bool isDashing = false;
     private float dashStartTime = 0.0f;
     private Vector3 currDashVelocity;
-    [SerializeField, ReadOnly] private float currentDashTime = 0.0f;
+    [SerializeField, ReadOnly] private float currDashTime = 0.0f;
     [SerializeField, Range(0.0f, 1.5f)] private float dashTime = 2.0f;
+    [SerializeField, Range(0.0f, 2.0f)] private float dashCooldown = 0.5f;
     private Vector3 dashDirection;
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+
+        currMoveSpeed = moveSpeed;
     }
     void Update()
     {
@@ -29,7 +34,7 @@ public class PlayerMovement : PlayerComponent
     }
     void FixedUpdate()
     {
-        if (!isDashing) playerRigidbody.linearVelocity = movementInput * moveSpeed;
+        if (!isDashing) playerRigidbody.linearVelocity = movementInput * CorrectedMoveSpeed();
         if (isDashing) Dash();
     }
     private void MoveCharacter()
@@ -37,30 +42,68 @@ public class PlayerMovement : PlayerComponent
         movementInput = moveAction.ReadValue<Vector2>();
         if (movementInput != Vector3.zero) lastmovementDirection = movementInput;
     }
-
+    [Range(0.0f, 5.0f)] public float speedRestoreRate = 1.0f;
+    [Range(0.0f, 5.0f)] public float speedDecayRate = 1.0f;
+    private float CorrectedMoveSpeed()
+    {
+        float unitsUnderLimit = moveSpeed - currMoveSpeed;
+        float unitsAboveLimit = currMoveSpeed - moveSpeed;
+        if (unitsUnderLimit > 0.1f)
+        {
+            float restoreMultiplier = Mathf.Clamp((unitsUnderLimit / moveSpeed), 1, 99);
+            currMoveSpeed += speedRestoreRate * restoreMultiplier * Time.fixedDeltaTime;
+        }
+        else if (unitsAboveLimit > 0.1f)
+        {
+            float decayMultiplier = Mathf.Clamp((unitsAboveLimit / moveSpeed), 1, 99);
+            currMoveSpeed -= speedDecayRate * decayMultiplier * Time.fixedDeltaTime;
+        }
+        else
+        {
+            currMoveSpeed = moveSpeed;
+        }
+        return currMoveSpeed;
+    }
     private void StartDash()
     {
+        // Dash still on Cooldown
+        if (Time.time - dashStartTime < dashCooldown) return;
+
         isDashing = true;
         dashStartTime = Time.time;
         dashDirection = (movementInput == Vector3.zero) ? lastmovementDirection : movementInput;
         currDashVelocity = dashDirection * 20;
     }
-    [Range(0.0f, 5.0f)] public float approach = 3.0f;
     private void Dash()
     {
-        currentDashTime = Time.time - dashStartTime;
-        if (currentDashTime >= dashTime)
+        currDashTime = Time.time - dashStartTime;
+        if (currDashTime >= dashTime)
         {
             StopDash();
             return;
         }
-        float coef = currentDashTime / dashTime;
+        float coef = currDashTime / dashTime;
 
         playerRigidbody.linearVelocity = currDashVelocity;
-        currDashVelocity = Vector3.Lerp(currDashVelocity, Vector3.zero, Time.deltaTime * approach);
+        currDashVelocity = Vector3.Lerp(currDashVelocity, Vector3.zero, Time.fixedDeltaTime * 3.0f);
     }
+    [SerializeField, Range(0.0f, 0.5f)] private float perfectDashLeniency = 0.05f;
     private void StopDash()
     {
+        if (!isDashing) return;
+
         isDashing = false;
+        // Perfect Dash
+        if (Mathf.Abs(0.5f*dashTime - currDashTime) <= perfectDashLeniency)
+        {
+            currMoveSpeed = Mathf.Clamp(currMoveSpeed * 1.5f, 0.0f, maxMoveSpeed);
+            dashStartTime = Time.time - (dashCooldown / 2.0f);
+        }
+        // Imperfect Dash
+        else
+        {
+            currMoveSpeed = Mathf.Clamp(currMoveSpeed - 5.0f, moveSpeed * 0.5f, maxMoveSpeed);
+            dashStartTime = Time.time;
+        }
     }
 }
