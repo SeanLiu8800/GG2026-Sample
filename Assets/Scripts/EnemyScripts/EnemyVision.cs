4bf156afad01b42d6cd436592c8970094f9c8f2d
SeanLiu8800 <tyson9963@gmail.com>
2026-04-04 09:01:06 -0700

    Enemy Vision now has a grace period
    -Enemy loses track if target is not seen for a specified amount of time

--- Assets/Scripts/EnemyScripts/EnemyVision.cs
+++ Assets/Scripts/EnemyScripts/EnemyVision.cs
@@ -5,6 +5,8 @@ public class EnemyVision : EnemyComponent
     [SerializeField] private Collider2D visionArea;
     [SerializeField] private LayerMask layerToDetect;
     [SerializeField] private LayerMask lineOfSightLayermask;
+
+    [SerializeField, Range(0.0f, 2.0f)] private float loseTargetTime = 1.0f;
     private void Start()
     {
         if (enemy.target != null) EnableVision();
@@ -37,20 +39,29 @@ public class EnemyVision : EnemyComponent
     {
         visionArea.enabled = false;
     }
-
+    
+    private float lastSeenTargetTime = -90.0f;
     private void CheckLineOfSight()
     {
         if (enemy.target == null) return;
         RaycastHit2D hit = Physics2D.Linecast(transform.position, enemy.target.transform.position, lineOfSightLayermask);
-        if (CheckIsTarget(hit.transform)) return;
-
-        Debug.Log("Lost sight of target");
-        enemy.target = null;
+        if (CheckIsTarget(hit.transform)) // Target can be seen
+        {
+            lastSeenTargetTime = Time.time;
+            return;
+        }
+        // Target CANNOT be seen
+        if (Time.time - lastSeenTargetTime >= loseTargetTime)
+        {
+            Debug.Log("Lost sight of target");
+            enemy.target = null;
+        }
     }
     /// <summary>
     /// Check if input Transform or any of it's Parents is this enemy's Target
     /// </summary>
     /// <param name="input">The Gameobject's Transform to examine</param>
+    /// <param name="target">The Gameobject to compare the input to, setting this to null defaults it to enemy.target</param>
     /// <returns>True if input IS or BELONGS TO the Target<br/>False otherwise</returns>
     private bool CheckIsTarget(Transform input, GameObject target = null)
     {
