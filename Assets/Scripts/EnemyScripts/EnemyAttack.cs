8ce5575315f8e4eff5c818309437ddb17cf64d4a
SeanLiu8800 <tyson9963@gmail.com>
2026-04-05 00:51:30 -0700

    Created EnemyAttackBase to reduce code redundancy

--- Assets/Scripts/EnemyScripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyScripts/EnemyAttack.cs
@@ -1,65 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
-public class EnemyAttack : EnemyComponent
+public class EnemyAttack : EnemyAttackBase
 {
-    [SerializeField] private Collider2D attackArea;
     [Header("Attack Variables")]
-    [SerializeField] private GameObject attackWarning;
     [SerializeField] private GameObject attack;
     [SerializeField] private GameObject meleeAttack;
-
-    [SerializeField, Range(0.0f, 5.0f)] private float attackCooldown = 1.0f;
-    protected virtual void OnEnable()
-    {
-        enemy.enemyEvents.onEnemyDies += OnEnemyDies;
-    }
-    protected virtual void OnDisable()
-    {
-        enemy.enemyEvents.onEnemyDies -= OnEnemyDies;
-    }
-
-    #region ----- Event Functions -----
-    protected virtual void OnEnemyDies()
-    {
-        StopAllAttacks();
-    }
-    #endregion
-    
-    void Update()
+    protected void Update()
     {
         Attack();
         if (Keyboard.current.tKey.wasPressedThisFrame) StartCoroutine(Shoot());
         if (Keyboard.current.gKey.wasPressedThisFrame) StartCoroutine(MeleeAttack());
     }
-    private void Attack()
+    protected override void Attack()
     {
         if (!enemy.allowAttack) return;
         if (!enemy.canAttack || enemy.target == null) return;
-
         enemy.canAttack = false;
-        if (Random.Range(0, 2) == 0) StartCoroutine(Shoot());
+
+        if (Random.Range(0, 2) == 1) StartCoroutine(Shoot());
         else StartCoroutine(MeleeAttack());
     }
-    public void AttackCooldown()
-    {
-        if (attackCooldownCoroutine != null) StopCoroutine(attackCooldownCoroutine);
-        attackCooldownCoroutine = StartCoroutine(AttackCooldownCoroutine());
-    }
-    private Coroutine attackCooldownCoroutine;
-    private IEnumerator AttackCooldownCoroutine()
-    {
-        enemy.canAttack = false;
-        float cooldownStartTime = Time.time;
-        while (Time.time - cooldownStartTime <= attackCooldown) yield return null;
-        enemy.canAttack = true;
-        yield break;
-    }
-    private void StopAllAttacks()
-    {
-        StopAllCoroutines();
-        AttackCooldown();
-    }    
     private IEnumerator Shoot()
     {
         GameObject shootTarget = enemy.target;
@@ -92,17 +53,7 @@ public class EnemyAttack : EnemyComponent
             1.0f
         );
         enemy.enemyRigidbody.AddForce(direction * 30.0f, ForceMode2D.Impulse);
-        float dashStartTime = Time.time;
-        while (Time.time - dashStartTime <= 0.3f)
-        {
-            if (enemy.IsTargetWithinDistance(2.0f))
-            {
-                Debug.Log("Enemy close enough to target");
-                enemy.enemyRigidbody.linearVelocity = Vector2.zero;
-                break;
-            }
-            yield return null;
-        }
+        yield return StartCoroutine(TrackDistanceToTarget(2.0f, 0.3f));
 
         SpawnAttack(meleeAttack, enemy.target, default, direction);
         enemy.enemyRigidbody.AddForce(direction * 10.0f, ForceMode2D.Impulse);
@@ -113,7 +64,7 @@ public class EnemyAttack : EnemyComponent
         SpawnAttack(meleeAttack, enemy.target, default, direction);
         enemy.enemyRigidbody.AddForce(direction * 10.0f, ForceMode2D.Impulse);
         yield return new WaitForSeconds(0.2f);
-        
+
         if (enemy.IsTargetWithinDistance(5.0f))
         {
             StartCoroutine(MeleeAttackFollowup());
@@ -143,28 +94,5 @@ public class EnemyAttack : EnemyComponent
 
         enemy.canMove = true;
         AttackCooldown();
-    }
-    private void SpawnAttack
-    (
-        GameObject attack, 
-        GameObject target, 
-        Vector3 initialLinearVelocity = default(Vector3), 
-        Vector3 lookDirection = default(Vector3)
-    )
-    {
-        GameObject currAttack = Instantiate(attack, transform.position, transform.rotation);
-        if (!currAttack.TryGetComponent<BulletScript>(out BulletScript currBulletScript))
-        {
-            Debug.LogWarning($"{this.attack.name} DOES NOT have a BulletScript");
-        }
-        else
-        {
-            currBulletScript.Initialize(
-                this.gameObject,
-                target,
-                initialLinearVelocity,
-                lookDirection
-            );
-        }
     }
 }
---
+++ Assets/Scripts/EnemyScripts/EnemyAttackBase.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using System.Collections;
+public abstract class EnemyAttackBase : EnemyComponent
+{
+    [Header("Enemy Attack Base Variables")]
+    [SerializeField] protected GameObject attackWarning;
+    [SerializeField, Range(0.0f, 5.0f)] protected float attackCooldown = 1.0f;
+
+    protected virtual void OnEnable()
+    {
+        enemy.enemyEvents.onEnemyDies += OnEnemyDies;
+    }
+    protected virtual void OnDisable()
+    {
+        enemy.enemyEvents.onEnemyDies -= OnEnemyDies;
+    }
+
+    #region ----- Event Functions -----
+    protected virtual void OnEnemyDies()
+    {
+        StopAllAttacks();
+    }
+    #endregion
+
+    protected abstract void Attack();
+
+    /// <summary>
+    /// Check to see whether target is within distance over the specified duration<br/>
+    /// Coroutine immediately ends if the target is within distance at any point<br/>
+    /// Coroutine will wait until the entire duration otherwise<br/>
+    /// </summary>
+    /// <param name="distance">The specified distance to compare target to</param>
+    /// <param name="duration">The duration to wait</param>
+    protected IEnumerator TrackDistanceToTarget(float distance, float duration)
+    {
+        float dashStartTime = Time.time;
+        while (Time.time - dashStartTime <= duration)
+        {
+            if (enemy.IsTargetWithinDistance(distance))
+            {
+                enemy.enemyRigidbody.linearVelocity = Vector2.zero;
+                yield break;
+            }
+            yield return null;
+        }
+    }
+    /// <summary>
+    /// Sets canAttack to true after this.attackCooldown amount of time
+    /// </summary>
+    protected void AttackCooldown()
+    {
+        if (attackCooldownCoroutine != null) StopCoroutine(attackCooldownCoroutine);
+        attackCooldownCoroutine = StartCoroutine(AttackCooldownCoroutine());
+    }
+    private Coroutine attackCooldownCoroutine;
+    private IEnumerator AttackCooldownCoroutine()
+    {
+        enemy.canAttack = false;
+        float cooldownStartTime = Time.time;
+        while (Time.time - cooldownStartTime <= attackCooldown) yield return null;
+        enemy.canAttack = true;
+        yield break;
+    }
+    /// <summary>
+    /// Stops all attacks (with StopAllCoroutines()) then puts this enemy on attack cooldoown
+    /// </summary>
+    protected void StopAllAttacks()
+    {
+        StopAllCoroutines();
+        AttackCooldown();
+    }    
+        /// <summary>
+    /// Spawn and then Initializes the given Bullet
+    /// </summary>
+    /// <param name="attack">The bullet to spawn</param>
+    /// <param name="target">The target for the bullet</param>
+    /// <param name="initialLinearVelocity">The Initial Linear Velocity of the Bullet</param>
+    /// <param name="lookDirection">The direction the Bullet will face</param>
+    protected void SpawnAttack
+    (
+        GameObject attack, 
+        GameObject target, 
+        Vector3 initialLinearVelocity = default(Vector3), 
+        Vector3 lookDirection = default(Vector3)
+    )
+    {
+        GameObject currAttack = Instantiate(attack, transform.position, transform.rotation);
+        if (!currAttack.TryGetComponent<BulletScript>(out BulletScript currBulletScript))
+        {
+            Debug.LogWarning($"{currAttack.name} DOES NOT have a BulletScript");
+        }
+        else
+        {
+            currBulletScript.Initialize(
+                this.gameObject,
+                target,
+                initialLinearVelocity,
+                lookDirection
+            );
+        }
+    }
+}
