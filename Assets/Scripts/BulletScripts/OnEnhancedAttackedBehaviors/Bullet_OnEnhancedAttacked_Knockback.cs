25bff2141381cc9b6552858beb6f89f0d1bda898
SeanLiu8800 <tyson9963@gmail.com>
2026-04-05 01:00:16 -0700

    Adjusted Bullet Knockback behavior
    -Increased it's range to 30.0f

--- Assets/Scripts/BulletScripts/OnEnhancedAttackedBehaviors/Bullet_OnEnhancedAttacked_Knockback.cs
+++ Assets/Scripts/BulletScripts/OnEnhancedAttackedBehaviors/Bullet_OnEnhancedAttacked_Knockback.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Bullet_OnEnhancedAttacked_Knockback : Bullet_OnEnhancedAttacked_BehaviorBase
 {
     [Header("Knockback Variables")]
-    [SerializeField, Range(0.0f, 10.0f)] private float knockbackStrength = 5.0f;
+    [SerializeField, Range(0.0f, 30.0f)] private float knockbackStrength = 10.0f;
     private void Start()
     {
         if (knockbackStrength <= 0) Debug.LogWarning($"{knockbackStrength} is Zero or Negative! Knockback will not happen!");
@@ -17,7 +17,6 @@ public class Bullet_OnEnhancedAttacked_Knockback : Bullet_OnEnhancedAttacked_Beh
             return;
         }
 
-        Vector3 direction = (enemy.transform.position - player.transform.position).normalized;
-        enemy.enemyRigidbody.AddForce(direction * knockbackStrength, ForceMode2D.Impulse);
+        enemy.enemyRigidbody.AddForce(-enemy.toTargetDirection * knockbackStrength, ForceMode2D.Impulse);
     }
 }
