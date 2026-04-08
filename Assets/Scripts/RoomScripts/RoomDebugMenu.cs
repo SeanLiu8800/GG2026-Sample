46a4ee4c354829b10469675ff62df0e9c429c0b0
SeanLiu8800 <tyson9963@gmail.com>
2026-04-08 15:00:14 -0700

    RoomDebugMenu can kill all wave enemies

--- Assets/Scripts/RoomScripts/RoomDebugMenu.cs
+++ Assets/Scripts/RoomScripts/RoomDebugMenu.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class RoomDebugMenu : RoomComponent
 {
+    private bool currState = false;
     private void Start()
     {
-        
+        SetActive(currState);
+    }
+    private void Update()
+    {
+        if (Keyboard.current.backquoteKey.wasPressedThisFrame) Toggle();
     }
     public void KillAllEnemies()
     {
         room.waveSpawner.KillCurrentEnemies();
+    }
+
+    private void Toggle()
+    {
+        currState = !currState;
+        SetActive(currState);
+    }
+    private void SetActive(bool input)
+    {
+        foreach (Transform currTransform in transform) currTransform.gameObject.SetActive(input);
     }
 }
