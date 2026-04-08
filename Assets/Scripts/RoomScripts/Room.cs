a820eb666688539d1b8a8d4f7fa29a70bb193cbb
SeanLiu8800 <tyson9963@gmail.com>
2026-04-08 15:52:10 -0700

    Began developing Room Camera

--- Assets/Scripts/RoomScripts/Room.cs
+++ Assets/Scripts/RoomScripts/Room.cs
@@ -4,6 +4,8 @@ public class Room : MonoBehaviour
 {
     public WaveSpawner waveSpawner { get; private set; }
     public RoomSpawnPoints spawnPoints { get; private set; }
+    public RoomCamera roomCamera { get; private set; }
+
     public RoomEventsStruct roomEvents;
 
     private void Awake()
@@ -16,8 +18,13 @@ public class Room : MonoBehaviour
         {
             Debug.LogError($"{this.name} DOES NOT have a RoomSpawnPoints Component!");
         }
+        if (!TryGetComponent<RoomCamera>(out RoomCamera _roomCamera))
+        {
+            Debug.LogError($"{this.name} DOES NOT have a RoomCamera Component!");
+        }
 
         waveSpawner = _waveSpawner;
         spawnPoints = _spawnPoints;
+        roomCamera = _roomCamera;
     }
 }
---
+++ Assets/Scripts/RoomScripts/RoomCamera.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class RoomCamera : RoomComponent
+{
+    private Camera mainCamera;
+    private GameObject player;
+
+    public bool isActive = true;
+    public bool followPlayer = true;
+    public bool stayInBounds = false;
+
+    [SerializeField] private GameObject boundsLower;
+    [SerializeField] private GameObject boundsUpper;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamera = Camera.main;
+        player = GameObject.Find("Player");
+
+        SanitizeBounds();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isActive) return;
+        Vector3 finalPos = player.transform.position;
+
+        if (followPlayer)
+        {
+            finalPos = player.transform.position;
+        }
+        if (stayInBounds)
+        {
+            finalPos.x = Mathf.Clamp(finalPos.x, boundsLower.transform.position.x, boundsUpper.transform.position.x);
+            finalPos.y = Mathf.Clamp(finalPos.y, boundsLower.transform.position.y, boundsUpper.transform.position.y);
+        }
+
+        mainCamera.transform.position = finalPos;
+    }
+
+    private void SanitizeBounds()
+    {
+        if (boundsLower == null)
+        {
+            Debug.LogError("Camera Lower Bound is Null! Falling back to this room's Position");
+            boundsLower = this.gameObject;
+        }
+        if (boundsUpper == null)
+        {
+            Debug.LogError("Camera Upper Bound is Null! Falling back to this room's Position");
+            boundsUpper = this.gameObject;
+        }
+        
+        float lowerX = boundsLower.transform.position.x;
+        float lowerY = boundsLower.transform.position.y;
+        float higherX = boundsUpper.transform.position.x;
+        float higherY = boundsUpper.transform.position.y;
+
+        boundsLower.transform.position = new Vector3(Mathf.Min(lowerX, higherX), Mathf.Min(lowerY, higherY), 0.0f);
+        boundsUpper.transform.position = new Vector3(Mathf.Max(lowerX, higherX), Mathf.Max(lowerY, higherY), 0.0f);
+    }
+}
