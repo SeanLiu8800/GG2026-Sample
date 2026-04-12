089c97649dc0f251c1597e5b89b6486159747178
SeanLiu8800 <tyson9963@gmail.com>
2026-04-11 20:43:15 -0700

    Created function to play BG Music
    -haven't tested it cause I haven't finished writing the BG Music LUL

--- Assets/Scripts/AudioScripts/AudioManager.cs
+++ Assets/Scripts/AudioScripts/AudioManager.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
-
+using System.Collections;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
     [SerializeField] private AudioSource audioSource;
+
+    [SerializeField] private AudioSource MusicIntroSource;
+    [SerializeField] private AudioSource MusicLoopSource;
+    [SerializeField] private AudioSource MusicInterludeSource;
+    [SerializeField] private AudioSource MusicOutroSource;
+
     [field : SerializeField] public SoundEffects soundEffects { get; private set; }
     private void Awake()
     {
@@ -15,6 +21,8 @@ public class AudioManager : MonoBehaviour
         {
             Instance = this;
         }
+
+        MusicLoopSource.loop = true;
     }
     
     public void PlaySoundOneShot(AudioClip audioClip)
@@ -30,4 +38,41 @@ public class AudioManager : MonoBehaviour
         Debug.LogError($"This Audio Clip is null/ unassigned!");
         return true;
     }
+
+    private Soundtrack currSoundtrack;
+    public void PlaySoundtrack(Soundtrack soundtrack)
+    {
+        currSoundtrack = soundtrack;
+
+        double startTime = AudioSettings.dspTime + 0.2;
+        double introDuration = 0.0;
+        if (soundtrack.intro == null)
+        {
+            Debug.LogWarning($"{soundtrack.name}'s intro component is NULL!");
+            introDuration = 0.0;
+        }
+        else
+        {
+            MusicIntroSource.clip = soundtrack.intro;
+            introDuration = soundtrack.intro.length;
+            MusicIntroSource.PlayScheduled(startTime);
+        }
+
+        if (soundtrack.mainLoop == null)
+        {
+            Debug.LogWarning($"{soundtrack.name}'s mainLoop component is NULL!");
+            if (soundtrack.intro != null)
+            {
+                Debug.LogWarning($"MainLoop falling back to Intro!");
+                MusicLoopSource.clip = soundtrack.intro;
+                MusicLoopSource.PlayScheduled(startTime + introDuration);
+            }
+        }
+        else
+        {
+            MusicLoopSource.clip = soundtrack.mainLoop;
+            MusicLoopSource.PlayScheduled(startTime + introDuration);
+        }
+    }
+    
 }
