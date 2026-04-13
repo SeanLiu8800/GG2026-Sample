28883b12395841af3be4babb7ecff7389ef78d41
SeanLiu8800 <tyson9963@gmail.com>
2026-04-13 16:43:40 -0700

    GameManager now spawns rooms

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     [SerializeField] private BiomeComponents biomeComponents;
+    [SerializeField] private Player player;
     [field: SerializeField] public Room currRoom { get; private set; }
+    [SerializeField, ReadOnly] int[] roomOrder;
     [field: SerializeField] public int roomNumber { get; private set; } = 0;
     private void Awake()
     {
@@ -21,16 +23,51 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        Fun();
+        StartGame();
     }
     private void StartGame()
     {
-
+        GenerateRoomOrder();
+        SpawnRoom();
+        
     }
-    private void Fun()
+    private void GenerateRoomOrder()
+    {
+        roomOrder = new int[biomeComponents.biomeLength];
+        int roomOrderIndex = 0;
+        while (roomOrderIndex < roomOrder.Length)
+        {
+            int[] currRoomOrder = new int[biomeComponents.biomeCombatRooms.Length];
+            for (int i = 0; i < biomeComponents.biomeCombatRooms.Length; i++) currRoomOrder[i] = i;
+            ShuffleArray(currRoomOrder);
+            for (int i = 0; i < currRoomOrder.Length; i++)
+            {
+                roomOrder[roomOrderIndex++] = currRoomOrder[i];
+                if (roomOrderIndex >= roomOrder.Length) return;
+            }
+        }
+    }
+    private void SpawnRoom()
+    {
+        if (currRoom != null)
+        {
+            Destroy(currRoom.gameObject);
+            Unsubscribe();
+        }
+        currRoom = Instantiate(biomeComponents.biomeCombatRooms[roomOrder[roomNumber]]).GetComponent<Room>();
+        player.transform.position = currRoom.spawnPoints.GetPlayerSpawnPoint();
+        Subscribe();
+    }
+
+    private void Subscribe()
     {
         currRoom.roomEvents.roomStarts += RoomStarts;
         currRoom.roomEvents.roomEnds += LoadNextRoom;
+    }
+    private void Unsubscribe()
+    {
+        currRoom.roomEvents.roomStarts -= RoomStarts;
+        currRoom.roomEvents.roomEnds -= LoadNextRoom;
     }
     private void RoomStarts()
     {
@@ -40,5 +77,21 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Game Manager sees that Room Ends");
         roomNumber ++;
+        if (roomNumber < roomOrder.Length) SpawnRoom();
+        else
+        {
+            roomNumber = -1;
+            Debug.Log("Biome Completed!");
+        }
+    }
+    private void ShuffleArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int temp = array[i];
+            int randomIndex = Random.Range(i, array.Length);
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
     }
 }
