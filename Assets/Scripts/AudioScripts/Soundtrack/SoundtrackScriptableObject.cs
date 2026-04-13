f26c2e57cab203339791f3d240da66ca4a9fe414
SeanLiu8800 <tyson9963@gmail.com>
2026-04-12 20:10:57 -0700

    Imported completed BG theme
    -Discovered that Musescore4 leaves 3.0f seconds after the end of an exported .wav/ .mp3 to let reverb ring out

--- Assets/Scripts/AudioScripts/Soundtrack/SoundtrackScriptableObject.cs
+++ Assets/Scripts/AudioScripts/Soundtrack/SoundtrackScriptableObject.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Soundtrack", menuName = "Scriptable Objects/Soundtrack")]
 public class Soundtrack : ScriptableObject
 {
+    [Range(0.0f, 200.0f)] public float tempo = 140.0f;
+
     public AudioClip intro;
     public AudioClip mainLoop;
     public AudioClip interlude;
