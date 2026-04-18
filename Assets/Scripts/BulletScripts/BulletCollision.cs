6b21ab08e711e55ae9e13ea1d9904de4f134716b
SeanLiu8800 <tyson9963@gmail.com>
2026-04-18 15:02:42 -0700

    cached wallLayer in BulletCollision

--- Assets/Scripts/BulletScripts/BulletCollision.cs
+++ Assets/Scripts/BulletScripts/BulletCollision.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class BulletCollision : BulletComponent
 {
+    private int wallLayer;
     private void Start()
     {
         if (bullet.interactLayer == 0) Debug.LogWarning($"{this.name}'s layerMask is set to Nothing! Should you set this to something?");
+        if ((wallLayer = LayerMask.NameToLayer("Wall")) == 0) Debug.LogError("COULD NOT find Wall Layer!");
     }
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.layer == wallLayer)
+        {
+            Vector3 normalVector = Physics2D.Raycast(transform.position, bullet.moveDirection).normal;
+            bullet.bulletEvents.onHitWall?.Invoke(normalVector);
+        }
         if (((1 << collision.gameObject.layer) & bullet.interactLayer) == 0) return;
-
         // If Bullet hits Player or Player's Attack area
         if (TryCheckIfPlayerCollider(collision, out Player player))
         {
@@ -20,11 +26,6 @@ public class BulletCollision : BulletComponent
         {
             iDamageable.Damage(bullet.damage);
             Destroy(this.gameObject);
-        }
-        else
-        {
-            Vector3 normalVector = Physics2D.Raycast(transform.position, bullet.moveDirection).normal;
-            bullet.bulletEvents.onHitWall?.Invoke(normalVector);
         }
     }
     /// <summary>
