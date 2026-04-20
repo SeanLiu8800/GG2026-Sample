e3cf4e938b1a5ad99762244e58ebd2ee64d9aad7
SeanLiu8800 <tyson9963@gmail.com>
2026-04-20 00:02:01 -0700

    created simple lunging attack enemy

--- Assets/Scripts/EnemyScripts/AttackScripts/EnemyAttack_Chaser.cs
+++ Assets/Scripts/EnemyScripts/AttackScripts/EnemyAttack_Chaser.cs
@@ -3,14 +3,20 @@ using System.Collections;
 public class EnemyAttack_Chaser : EnemyAttackBase
 {
     [Header("Attack Variables")]
+    [SerializeField, Range(1.0f, 5.0f)] private float attackRange = 2.0f;
     [SerializeField] private GameObject attack;
 
+    private void Update()
+    {
+        Attack();
+    }
     protected override void Attack()
     {
         if (!enemy.allowAttack) return;
         if (enemy.isBeingPummeled || enemy.isParryStunned) return;
         if (!canAttack || enemy.isAttacking || enemy.target == null) return;
 
+        if (!enemy.IsTargetWithinDistance(attackRange)) return;
         enemy.isAttacking = true;
         canAttack = false;
         switch (Random.Range(0, 1))
@@ -26,6 +32,28 @@ public class EnemyAttack_Chaser : EnemyAttackBase
 
     private IEnumerator LungeAndAttack()
     {
-        yield break;
+        enemy.canMove = false;
+
+        Vector3 direction = enemy.toTargetDirection;
+        float dist = 2.0f;
+        AttackZoneManager.Instance.SetSquareAttackZone(
+            transform.position + direction * dist,
+            direction,
+            3.0f,
+            2.0f * dist,
+            1.0f
+        );
+        AttackWarning();
+        yield return new WaitForSeconds(0.2f);
+
+        
+        enemy.move.Dash(direction, dist);
+        SpawnAttack(attack, enemy.target, default, direction);
+
+        yield return new WaitForSeconds(0.2f);
+
+        enemy.canMove = true;
+        enemy.isAttacking = false;
+        AttackCooldown();
     }
 }
