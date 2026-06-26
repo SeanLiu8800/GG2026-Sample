2aa908a7e7944078b2b9495afd01bc751d687ccb
SeanLiu8800 <tyson9963@gmail.com>
2026-06-25 19:20:20 -0700

    changed bullet damage collision logic
    -now can hurt the same target at most a predefined amount of times (adjustable with perTargetHitCount in BulletCollision.cs)
    -reorganized placeholder enemy bullets into a subfolder

--- Assets/Scripts/BulletScripts/BulletCollision.cs
+++ Assets/Scripts/BulletScripts/BulletCollision.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BulletCollision : BulletComponent
 {
     private int wallLayer;
+    [SerializeField] private Dictionary<GameObject, int> damagedGameObjects;
+    [Tooltip("Controls how many times this same exact bullet can hit the same GameObject")]
+    [SerializeField, Range(0, 3)] private int perTargetHitCount = 1;
     private void Start()
     {
         if (bullet.damageLayer == 0) Debug.LogWarning($"{this.name}'s layerMask is set to Nothing! Should you set this to something?");
         if ((wallLayer = LayerMask.NameToLayer("Wall")) == 0) Debug.LogError("COULD NOT find Wall Layer!");
+
+        damagedGameObjects = new Dictionary<GameObject, int>(10);
     }
     
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,9 +23,17 @@ public class BulletCollision : BulletComponent
             bullet.bulletEvents.onHitWall?.Invoke(normalVector);
         }
         if (((1 << collision.gameObject.layer) & bullet.damageLayer) == 0) return;
-        // If Bullet hits Player
+        // If Bullet hits a Damageable GameObject
         IDamageable damageable = collision.GetComponentInParent<IDamageable>();
-        if (damageable != null) damageable.BulletHits(bullet);
+        bool hasHitBefore = damagedGameObjects.TryGetValue(collision.gameObject, out int hitCount);
+        if (damageable != null && (!hasHitBefore || hitCount < perTargetHitCount))
+        {
+            if (!hasHitBefore) damagedGameObjects.Add(collision.gameObject, 1);
+            else damagedGameObjects[collision.gameObject]++;
+            foreach (KeyValuePair<GameObject, int> pair in damagedGameObjects) Debug.Log($"{pair.Key.name}, was hit {pair.Value} times");
+
+            damageable.BulletHits(bullet);
+        }
         //else Debug.LogWarning($"Bullet hits {collision.name}, which isn't damageable!");
     }
 }
