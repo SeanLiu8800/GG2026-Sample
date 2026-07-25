a9b20a55fd5d24cb405d5a24f3cf63eba8925dde
SeanLiu8800 <tyson9963@gmail.com>
2026-07-25 16:25:44 -0700

    Created ActionSlowdown onDashedInto Behavior

--- Assets/Scripts/BulletScripts/OnDashedIntoBehaviors/Bullet_OnDashedInto_ActionSlowdown.cs
+++ Assets/Scripts/BulletScripts/OnDashedIntoBehaviors/Bullet_OnDashedInto_ActionSlowdown.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class Bullet_OnPlayerAttacked_ActionSlowdown : Bullet_OnPlayerAttacked_BehaviorBase
+public class Bullet_OnDashedInto_ActionSlowdown : Bullet_OnDashedInto_BehaviorBase
 {
     [Header("Action Slowdown Variables")]
     [SerializeField, Range(0.0f, 1.0f)] private float easeInDuration = 0.1f;
     [SerializeField, Range(0.0f, 1.0f)] private float slowdownDuration = 0.1f;
     [SerializeField, Range(0.0f, 1.0f)] private float easeOutDuration = 0.2f;
-    protected override void OnPlayerAttackedBehavior(Player player)
+    protected override void OnDashedInto(Player player)
     {
         SlowdownManager.Instance.ActionSlowdown(easeInDuration, slowdownDuration, easeOutDuration);
     }
